11b707edd3f2f2eaaa949fb56a63384a98082083
ZMR <zmenrain@protonmail.com>
2026-02-26 09:58:44 +0200

    removed a lot of the feeds for facebook marketplace

--- entrypoints/facebook.content/index.ts
+++ entrypoints/facebook.content/index.ts
@@ -6,6 +6,7 @@ const configKeys: StorageItemKey[] = [
   "local:facebook-hide-games",
   "local:facebook-hide-main-feed",
   "local:facebook-hide-videos",
+  "local:facebook-marketplace-searchbox",
 ];
 
 export default defineContentScript({
@@ -21,7 +22,11 @@ export default defineContentScript({
 function NoParams(url: string): string {
   let obj = new URL(url);
 
-  return obj.origin + obj.pathname;
+  url = obj.origin + obj.pathname;
+
+  if (url[url.length - 1] != "/") url += "/";
+
+  return url;
 }
 
 function unfeeder() {
@@ -34,7 +39,32 @@ function unfeeder() {
     url.includes("https://www.facebook.com/watch/") ||
     url.includes("https://www.facebook.com/reel/");
 
+  let marketPage = isMarketplaceUrlTrue(url);
+
   root?.setAttribute("page-gaming", String(gamingPage));
   root?.setAttribute("page-home", String(homepage));
   root?.setAttribute("page-watch", String(videoPage));
+  root?.setAttribute("page-marketplace", String(marketPage));
+}
+
+export function isMarketplaceUrlTrue(urlString: string): boolean {
+  try {
+    // Parse the URL; this will throw if the string is not a valid URL.
+    const url = new URL(urlString);
+    const path = url.pathname;
+
+    const notFeedPattern =
+      /marketplace\/(inbox|notifications|status|you|item|groups)/;
+    if (notFeedPattern.test(path)) {
+      return false;
+    }
+
+    if (path.includes("search")) {
+      return false;
+    }
+
+    return true;
+  } catch {
+    return false;
+  }
 }
