3f136c364a817e31cbf35ce6bab9df6b304870d9
ZMR <116457838+ZMensRain@users.noreply.github.com>
2026-03-28 17:58:44 +0200

    added dropdown for configuring a platform quickly. added function that simplifies creating boolean keys

--- components/option.tsx
+++ components/option.tsx
@@ -1,6 +1,10 @@
 import { ConfigurationKey } from "../utils/Config";
 
-type Props = {} & ConfigurationKey;
+type Props = {
+  config: ConfigurationKey;
+  value: string;
+  onChange: (value: string) => void;
+};
 
 function isBooleanOption(options: string[]): boolean {
   const s = new Set(options);
@@ -12,36 +16,25 @@ function isBooleanOption(options: string[]): boolean {
 }
 
 export const ConfigOption = (props: Props) => {
-  const resource = createResource(async () =>
-    storage.getItem<string>(props.Key)
-  );
-  const value = () => resource[0]() ?? props.Values[0];
-
   return (
     <div class="flex flex-row justify-between items-center overflow-auto">
-      <label for={props.Key}>{props.HumanName}</label>
-      {isBooleanOption(props.Values) ? (
+      <label for={props.config.Key}>{props.config.HumanName}</label>
+      {isBooleanOption(props.config.Values) ? (
         <input
           type="checkbox"
-          id={props.Key}
-          checked={value() == "true"}
-          onChange={(e) => {
-            storage.setItem(props.Key, String(e.target.checked));
-            resource[1].refetch();
-          }}
+          id={props.config.Key}
+          checked={props.value == "true"}
+          onChange={(e) => props.onChange(String(e.target.checked))}
           class="w-4.5 h-4.5 border rounded-md accent-primary"
         />
       ) : (
         <select
-          id={props.Key}
-          onChange={(e) => {
-            storage.setItem(props.Key, e.target.value);
-            resource[1].refetch();
-          }}
+          id={props.config.Key}
+          onChange={(e) => props.onChange(e.target.value)}
           class="border-2 p-1.5 rounded-md"
         >
-          <For each={props.Values}>
-            {(opt) => <option selected={value() == opt}>{opt}</option>}
+          <For each={props.config.Values}>
+            {(opt) => <option selected={props.value == opt}>{opt}</option>}
           </For>
         </select>
       )}
--- components/optionSection.tsx
+++ components/optionSection.tsx
@@ -6,19 +6,120 @@ type Props = {
 };
 
 export const ConfigSection = (props: Props) => {
+  const [values, resource] = createResource(async () => {
+    return Promise.all(
+      props.config.Keys.map(async (key) => ({
+        config: key,
+        value: await storage.getItem(key.Key, { fallback: key.Values[0] }),
+      }))
+    );
+  });
+
+  const isMaxRequired = () => {
+    for (const value of props.config.Keys) {
+      if (value.Values[0] !== value.Max) return true;
+    }
+    return false;
+  };
+
+  const currentSetting = () => {
+    const state = values() ?? [];
+    let isDefault = true;
+    let isMax = true;
+    for (const value of state) {
+      if (isDefault && value.value != value.config.Values[0]) isDefault = false; // cannot be default anymore
+      if (
+        isMax &&
+        value.value != value.config.Max &&
+        value.value != value.config.Values[0]
+      )
+        isMax = false; // cannot be max anymore
+    }
+
+    if (isMax && isDefault && !isMaxRequired()) return "Default";
+    if (isDefault) return "Default";
+    if (isMax) return "Max";
+    return "Custom";
+  };
+
+  function onChange(n: string) {
+    if (n == "Custom") return;
+
+    if (n == "Default") {
+      const items = (values() ?? []).map(({ config }) => ({
+        key: config.Key,
+        value: config.Values[0],
+      }));
+      storage.setItems(items);
+      resource.refetch();
+    }
+
+    if (n == "Max") {
+      const items = (values() ?? []).map(({ config }) => ({
+        key: config.Key,
+        value: config.Max,
+      }));
+      storage.setItems(items);
+      resource.refetch();
+    }
+  }
+
   return (
     <section
       data-key={props.key}
       class="break-inside-avoid p-4 flex flex-col gap-2"
     >
-      <h2 class="text-3xl font-bold">{props.config.HumanName}</h2>
+      <div class="flex justify-between">
+        <h2 class="text-3xl font-bold">{props.config.HumanName}</h2>
+        <QuickSettingsButton
+          name={props.key}
+          currentSetting={currentSetting()}
+          isMaxRequired={isMaxRequired()}
+          onChange={onChange}
+        />
+      </div>
       <For
-        each={props.config.Keys.toSorted((a, b) =>
-          a.HumanName.localeCompare(b.HumanName)
+        each={(values() ?? []).toSorted((a, b) =>
+          a.config.HumanName.localeCompare(b.config.HumanName)
         )}
       >
-        {(option) => <ConfigOption {...option} />}
+        {(option) => (
+          <ConfigOption
+            {...option}
+            onChange={(newValue) => {
+              storage.setItem(option.config.Key, newValue);
+              resource.refetch();
+            }}
+          />
+        )}
       </For>
     </section>
   );
 };
+
+type props = {
+  name: string;
+  isMaxRequired: boolean;
+  currentSetting: string;
+  onChange: (setting: string) => void;
+};
+
+function QuickSettingsButton(props: props) {
+  const options = [
+    "Default",
+    ...(props.isMaxRequired ? ["Max"] : []),
+    "Custom",
+  ];
+
+  return (
+    <select
+      onChange={(e) => props.onChange(e.target.value)}
+      class="p-1.5 rounded-md"
+      aria-label={props.name + " templates"}
+    >
+      <For each={options}>
+        {(opt) => <option selected={props.currentSetting == opt}>{opt}</option>}
+      </For>
+    </select>
+  );
+}
