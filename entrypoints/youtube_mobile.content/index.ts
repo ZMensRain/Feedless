b2b19cd2d650f8b7db19771e518ecce05965add6
ZMR <116457838+ZMensRain@users.noreply.github.com>
2026-04-03 14:01:31 +0200

    youtube mobile support fixed #44

---
+++ entrypoints/youtube_mobile.content/index.ts
@@ -0,0 +1,39 @@
+import "./youtube_mobile.scss";
+
+let hideNextFeed = "false";
+let shortform = "show";
+
+export default defineContentScript({
+  matches: ["*://m.youtube.com/*"],
+  runAt: "document_start",
+  main(ctx) {
+    Config(ConfigurationShape["www.youtube.com"], onUpdate);
+    NewObserver(unfeeder, ctx);
+    AddNoScroll(scrollBlockerActive, ["ArrowDown", "ArrowUp"]);
+    unfeeder();
+  },
+});
+
+function scrollBlockerActive(event: Event) {
+  const inComments =
+    (event.target as HTMLElement).closest("panel-container") !== null;
+
+  return document.URL.includes("shorts") && shortform !== "show" && !inComments;
+}
+
+function onUpdate(key: string, value: string) {
+  if (key === "local:youtube-shortform") shortform = value;
+  if (key === "local:youtube-hide-up-next-feed") hideNextFeed = value;
+}
+
+function unfeeder() {
+  AddPath();
+
+  if (hideNextFeed === "true") {
+    (
+      document.querySelector(
+        "button.ytm-autonav-toggle-button-container[aria-pressed='true']"
+      ) as HTMLButtonElement | undefined
+    )?.click();
+  }
+}
--- utils/AddNoScroll.ts
+++ utils/AddNoScroll.ts
@@ -38,6 +38,7 @@ export default function AddNoScroll(
     "dragstart",
     "dragend",
     "drag",
+    "touchmove",
   ];
   events.forEach((eventType) =>
     window.addEventListener(eventType, preventScroll, {
