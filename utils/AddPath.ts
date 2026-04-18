2f9fbfc82c1032882c22781f0ad13a4129cb639e
ZMR <116457838+ZMensRain@users.noreply.github.com>
2026-04-18 17:32:43 +0200

    Mobile support (#52)
    * youtube mobile support fixed #44
    
    * mobile pinterest fixes #43
    
    * added utils to make adding support for mobile easier
    
    * added tiktok support #45
    
    * linkedin mobile support with slightly better premium hiding #46
    
    * hide shorts tab on channel on mobile youtube

--- utils/AddPath.ts
+++ utils/AddPath.ts
@@ -1,7 +1,10 @@
-export default function AddPath() {
+export function GetPath() {
   let url = new URL(document.URL);
   let path = url.pathname + "/";
   path = path.replaceAll("//", "/");
+  return path;
+}
 
-  document.querySelector(":root")?.setAttribute("page-path", path);
+export default function AddPath() {
+  document.querySelector(":root")?.setAttribute("page-path", GetPath());
 }
